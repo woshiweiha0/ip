1ee17c18cf9885a1c2ee4aec759435c737added6
woshiweihao <woshiweihao03@gmail.com>
2026-01-20 02:35:23 +0800

    Level 0. Rename, Greet, Exit

---
+++ src/main/java/Spike.java
@@ -0,0 +1,7 @@
+public class Spike {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Spike");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
