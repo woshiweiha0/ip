ec2eb48a038fb9c0fe148eb9a7a388005ecb42ea
woshiweihao <woshiweihao03@gmail.com>
2026-01-26 16:28:50 +0800

    Level-3: Mark as Done

--- src/main/java/Spike.java
+++ src/main/java/Spike.java
@@ -4,8 +4,8 @@ public class Spike {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String[] items = new String[100];
-        int itemCount = 0;
+        Task[] tasks = new Task[100];
+        int taskCount = 0;
 
         printLine();
         System.out.println("Hello! I'm Spike");
@@ -24,18 +24,43 @@ public class Spike {
 
             if (input.equals("list")) {
                 printLine();
-                for (int i = 0; i < itemCount; i += 1) {
-                    System.out.println((i + 1) + ". " + items[i]);
+                System.out.println("Here are the tasks in your list:");
+                for (int i = 0; i < taskCount; i += 1) {
+                    System.out.println((i + 1) + ".[" + tasks[i].getStatusIcon() + "] " + tasks[i].getDescription());
                 }
                 printLine();
-            } else {
-                items[itemCount] = input;
-                itemCount += 1;
+                continue;
+            }
+
+            if (input.startsWith("mark ")) {
+                int index = Integer.parseInt(input.substring(5)) - 1;
+                tasks[index].markAsDone();
 
                 printLine();
-                System.out.println("added: " + input);
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("[X] " + tasks[index].getDescription());
                 printLine();
+                continue;
             }
+
+            if(input.startsWith("unmark ")) {
+                int index = Integer.parseInt(input.substring(7)) - 1;
+                tasks[index].unmark();
+
+                printLine();
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("[ ] " + tasks[index].getDescription());
+                printLine();
+                continue;
+            }
+
+            tasks[taskCount] = new Task(input);
+            taskCount += 1;
+
+            printLine();
+            System.out.println("added: " + input);
+            printLine();
+
         }
 
         scanner.close();
---
+++ src/main/java/Task.java
@@ -0,0 +1,25 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void markAsDone() {
+        isDone = true;
+    }
+
+    public void unmark() {
+        isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
