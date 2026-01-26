c0aa98aeb6d8eb5af6fed7a54853913d386fe567
woshiweihao <woshiweihao03@gmail.com>
2026-01-26 12:12:32 +0800

    Level-2: Add, list

--- src/main/java/Spike.java
+++ src/main/java/Spike.java
@@ -4,6 +4,9 @@ public class Spike {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String[] items = new String[100];
+        int itemCount = 0;
+
         printLine();
         System.out.println("Hello! I'm Spike");
         System.out.println("What can I do for you?");
@@ -19,9 +22,20 @@ public class Spike {
                 break;
             }
 
-            printLine();
-            System.out.println(input);
-            printLine();
+            if (input.equals("list")) {
+                printLine();
+                for (int i = 0; i < itemCount; i += 1) {
+                    System.out.println((i + 1) + ". " + items[i]);
+                }
+                printLine();
+            } else {
+                items[itemCount] = input;
+                itemCount += 1;
+
+                printLine();
+                System.out.println("added: " + input);
+                printLine();
+            }
         }
 
         scanner.close();
