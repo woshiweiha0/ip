b0cc15ebf6a92777d8dd535ffa5d3e8b1bbe05f9
woshiweihao <woshiweihao03@gmail.com>
2026-01-26 11:53:33 +0800

    Level-1: echo

--- src/main/java/Spike.java
+++ src/main/java/Spike.java
@@ -1,7 +1,33 @@
+import java.util.Scanner;
+
 public class Spike {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        printLine();
         System.out.println("Hello! I'm Spike");
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        printLine();
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                printLine();
+                System.out.println("Bye. Hope to see you again soon!");
+                printLine();
+                break;
+            }
+
+            printLine();
+            System.out.println(input);
+            printLine();
+        }
+
+        scanner.close();
+    }
+
+    private static void printLine() {
+        System.out.println("__________________________________________");
     }
 }
